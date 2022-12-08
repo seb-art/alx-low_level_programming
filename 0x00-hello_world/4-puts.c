f75cfd698425d4227958926c9727f2b34f783e9d
seb-art <kimanzikims300@gmail.com>
2022-12-08 04:00:45 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,18 @@
 
 /**
  *
- *  * main - main block
+ * main - main block
  *
- *   * Return: 0
+ * Return: 0
  *
- *    */
+ */
 
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 
 }
