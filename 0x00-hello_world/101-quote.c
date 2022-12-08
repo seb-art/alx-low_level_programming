08610a1c211f62103de23ccf918e8000cc82e4ed
seb-art <kimanzikims300@gmail.com>
2022-12-08 04:55:26 -0800

    simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,9 @@ int main(void)
 
 {
 	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	write("and that piece of art is useful","- Dora Korpar, 2015-10-19 \n")
+	write(2,
+	     "and that piece of art is useful - Dora Korpar, 2015-10-19 \n",
+	     s)
+
 	return (1);
 }
