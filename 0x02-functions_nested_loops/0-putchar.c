4f933affcc1500c0f80a9c61f673a89df21a84d3
seb-art <kimanzikims300@gmail.com>
2022-12-14 11:38:10 -0800

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ *main - this is an entry point to the program
+ *description : prints putchar follows by a new line
+ *Return : return 0 when successful
+ */
+
+int main(void)
+{
+	printf("_putchar", \n);
+	
+	return(0);
+}
