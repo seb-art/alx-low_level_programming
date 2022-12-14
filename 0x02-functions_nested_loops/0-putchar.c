502237673285ea99094c74e024613969ec5aa0b3
seb-art <kimanzikims300@gmail.com>
2022-12-14 11:51:02 -0800

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,15 @@
 
 int main(void)
 {
-	printf("_putchar", \n);
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
 	
 	return(0);
 }
