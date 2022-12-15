b406f9ab683ef09d4bb51dc63158af859c5b4a94
seb-art <kimanzikims300@gmail.com>
2022-12-15 14:07:05 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - entrypoint of the code
@@ -8,14 +8,14 @@
 
 int main (void)
 {
-	char putchar = "_putchar";
-	int i = 0;
-
-	while(putchar[i] != '\0');
-	{
-		_putchar(putchar[i]);
-		i++;
-	}
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	_putchar("\n");
 
 	return(0);
