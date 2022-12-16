9c9d24a76ab9b514507913e67c2360f0403e26cf
seb-art <kimanzikims300@gmail.com>
2022-12-16 04:16:12 -0800

    prints 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include "main.h"
 
 /**
@@ -12,8 +13,8 @@ void print_numbers(void)
 	j = 0;
 	while (j <10)
 	{
-		_putchar(j);
+		putchar(j);
 		j++
 	}
-	_putchar("\n");
+	putchar("\n");
 }
