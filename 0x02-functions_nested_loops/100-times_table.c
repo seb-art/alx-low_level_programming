556158c2471abb0ea901dfddd682eb44867ebc22
seb-art <kimanzikims300@gmail.com>
2023-06-05 04:37:44 -0700

    nested loops advanced tasks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - func to print times table with
+ * parameter
+ * Return: return nothing
+ * @n: integer for the arguement
+ */
+void print_times_table(int n)
+{
+	int num, mult, result;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				result = num * mult;
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
