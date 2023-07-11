afa9d286c0428086877e53f846bd8468794a18b6
seb-art <kimanzikims300@gmail.com>
2023-07-11 12:06:51 -0700

    a function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,28 +1,29 @@
 #include "search_algos.h"
-
 /**
- * linear_search - Searches for a value in an array of integers
- *                  using the Linear search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: Index of the first occurrence of value in array,
- *         or -1 if value is not present in array or if array is NULL
- */
+* linear_search - a function that searches for a value in an
+*array of integers using the Linear search algorithm
+* @array: is a pointer to the first element of the array to search in
+* @size: is the number of elements in array
+* @value: is the value to search for
+* Return: index OR -1
+**/
 int linear_search(int *array, size_t size, int value)
 {
-
-	int i;
+	unsigned int i;
+	int flag = 0;
 
 	if (array == NULL)
 		return (-1);
-
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		if (array[i] == value)
+		{
+			flag = 1;
 			return (i);
+		}
 	}
-	return (-1);
+	if (flag != 1)
+		return (-1);
+	return (i);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,55 +1,35 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-} listint_t;
+/* helpers */
+void print_array(int *array, size_t inc, size_t size);
 
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- */
 typedef struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
 } skiplist_t;
 
-/*function that searches for a value in an array using the linear search algorithm*/
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
+
+/* main functions */
 int linear_search(int *array, size_t size, int value);
-/*function that searches for a value in a sorted array of integers using the binary search algorithm*/
 int binary_search(int *array, size_t size, int value);
-/*function that searches for a value in a sorted array of integers using the Jump search algorithm*/
 int jump_search(int *array, size_t size, int value);
-/*function that searches for a value in a sorted array of integers using the Interpolation search algorithm*/
 int interpolation_search(int *array, size_t size, int value);
-/*function that searches for a value in a sorted array of integers using the Exponential search algorithm*/
 int exponential_search(int *array, size_t size, int value);
-/*function that searches for a value in a sorted array of integers*/
 int advanced_binary(int *array, size_t size, int value);
+
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-#endif /* SEARCH_ALGOS_H */
+#endif
